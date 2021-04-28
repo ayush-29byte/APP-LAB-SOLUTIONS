106d60530f546f7ecadb62ff22eec7601818c659
Ayush Saxena <62539392+ayush-29byte@users.noreply.github.com>
2021-04-28 08:50:39 +0530

    Add files via upload

---
+++ imperative 1.py
@@ -0,0 +1,12 @@
+#CONSTRUCT A PATTERN USING A NESTED FOR LOOP
+n=5;
+for i in range(n):
+    for j in range(i):
+        print ('* ', end="")
+    print('')
+
+for i in range(n,0,-1):
+    for j in range(i):
+        print('* ', end="")
+    print('')
+#CODE BY AYUSH SAXENA
---
+++ imperative 2.py
@@ -0,0 +1,7 @@
+#FIBONACCI SERIES BETWEEN 0 TO 50
+x,y=0,1
+
+while y<50:
+    print(y)
+    x,y = y,x+y
+#CODE BY AYUSH SAXENA
